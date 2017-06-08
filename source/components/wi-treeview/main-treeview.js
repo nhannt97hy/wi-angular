4dff8dd3960f3263a2e9ae9691695cca690c96a0
tienm <tienminh.uet@gmail.com>
2017-06-08 12:01:22 +0700

    wi-treeview

--- source/components/wi-treeview/main-treeview.js
+++ source/components/wi-treeview/main-treeview.js
@@ -2,5 +2,96 @@ wiTreeview = require('./wi-treeview');
 
 var app = angular.module('helloapp', ['ui.bootstrap', wiTreeview.name]);
 app.controller('WiDummy', function($scope) {
-
+    $scope.myConfig = TREE_CONFIG_TEST;
 });
+
+const TREE_CONFIG_TEST = [
+    {
+        data: {
+            imgUrl: 'img/32x32/project_new_32x32.png',
+            label: 'item 1',
+            description: 'description 1',
+            childExpanded: false,
+            handler: ''
+        },
+        children: [
+            {
+                data: {
+                    imgUrl: 'img/32x32/project_new_32x32.png',
+                    label: 'item 1.1',
+                    description: '',
+                    childExpanded: true,
+                    handler: ''
+                },
+                children: [
+                    {
+                        data: {
+                            imgUrl: 'img/32x32/project_new_32x32.png',
+                            label: 'item 1.2.1',
+                            description: '',
+                            childExpanded: true,
+                            handler: ''
+                        },
+                        children: [
+                            {
+                                data: {
+                                    imgUrl: 'img/32x32/project_new_32x32.png',
+                                    label: 'item 1.2.1.1',
+                                    description: '',
+                                    childExpanded: true,
+                                    handler: ''
+                                },
+                                children: [
+                                    {
+                                        data: {
+                                            imgUrl: 'img/32x32/project_new_32x32.png',
+                                            label: 'item 1.2.1.1.1',
+                                            description: '',
+                                            childExpanded: true,
+                                            handler: ''
+                                        },
+                                        children: [
+
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        data: {
+                            imgUrl: 'img/32x32/project_new_32x32.png',
+                            label: 'item 1.2.2',
+                            description: '',
+                            childExpanded: true,
+                            handler: ''
+                        },
+                        children: [
+
+                        ]
+                    }
+                ]
+            },
+            {
+                data: {
+                    imgUrl: 'img/32x32/project_new_32x32.png',
+                    label: 'item 1.2',
+                    description: '',
+                    childExpanded: true,
+                    handler: ''
+                },
+                children: []
+            }
+        ]
+    },
+    {
+        data: {
+            imgUrl: 'img/32x32/project_new_32x32.png',
+            label: 'item 2',
+            description: 'description 2',
+            childExpanded: true,
+            handler: ''
+        },
+        children: []
+    }
+];
--- source/components/wi-treeview/wi-treeview.js
+++ source/components/wi-treeview/wi-treeview.js
@@ -3,12 +3,23 @@ const moduleName = 'wi-treeview';
 
 function Controller() {
     var self = this;
+
+    this.$onInit = function () {
+        console.log('config: ', self.config);
+    }
+
+    this.onCaretClick = function () {
+        self.data.childExpanded = !self.data.childExpanded;
+    }
 }
 var app = angular.module(moduleName, []);
 app.component(componentName, {
     templateUrl: 'wi-treeview.html',
     controller: Controller,
-    controllerAs: componentName
+    controllerAs: componentName,
+    bindings: {
+        config: '<'
+    }
 });
 
-exports.name=moduleName;
+exports.name = moduleName;
