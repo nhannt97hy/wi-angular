e739f340b768733c02ef370d225b403c2e0f4918
tienm <tienminh.uet@gmail.com>
2017-06-12 13:23:26 +0700

    dropdown button

--- source/main.js
+++ source/main.js
@@ -1,10 +1,11 @@
 wiButton = require('./wi-button.js');
+wiDropdown = require('./wi-dropdown.js');
 wiToolbar = require('./wi-toolbar.js');
 wiTabs = require('./wi-tabs.js');
 wiTreeview = require('./wi-treeview');
 wiStatusBar = require('./wi-status-bar');
 
-var app = angular.module('wiapp', [wiButton.name, wiToolbar.name, wiTabs.name, wiTreeview.name, wiStatusBar.name]);
+var app = angular.module('wiapp', [wiButton.name, wiDropdown.name, wiToolbar.name, wiTabs.name, wiTreeview.name, wiStatusBar.name]);
 
 app.controller('AppController', function ($scope) {
     $scope.myConfig = TREE_CONFIG_TEST;
