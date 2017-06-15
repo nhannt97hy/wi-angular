c3d26a980242e8a8f519d3e8a9de4b6939f30062
tienm <tienminh.uet@gmail.com>
2017-06-15 17:49:39 +0700

    config wi-toolbar

--- source/components/wi-toolbar/main-toolbar.js
+++ source/components/wi-toolbar/main-toolbar.js
@@ -1,26 +1,7 @@
 wiRibbon = require('./wi-button.js');
 wiToolbar = require('./wi-toolbar.js');
 
-app=angular.module('helloapp', ['ui.bootstrap', wiToolbar.name, wiRibbon.name]);
-app.controller('WiDummy', function($scope) {
-        $scope.buttonCfg = buttonCfg;
-        $scope.buttonCfg2 = buttonCfg2;
-    });
-buttonCfg = {
-    icon: 'project-new-32x32',
-    label: 'New Project',
-    layout: 'icon-left',
-    handlers: {
-        onclick: function() {
-            console.log('click');
-        },
-        onmouseover: function() {
-            console.log('mouseOver');
-        }
-    }
-};
-buttonCfg2 = {
-    icon: 'project-open-32x32',
-    label: 'Open Project',
-    layout: 'icon-top'
-};
+app = angular.module('helloapp', [wiToolbar.name, wiRibbon.name]);
+app.controller('WiDummy', function ($scope) {
+
+});
