03ed0b2aca3c119000d146ec171432769bf162db
minhnt58 <tienminh.uet@gmail.com>
2017-06-19 17:36:47 +0700

    wi-logplot: done separate layout with wi-slidingbar

--- source/app/components/wi-logplot/main-logplot.js
+++ source/app/components/wi-logplot/main-logplot.js
@@ -1,9 +1,6 @@
-wiButton = require('./wi-button.js');
-wiToolbar = require('./wi-toolbar.js');
-wiSlidingbar = require('./wi-slidingbar.js');
 wiLogplot = require('./wi-logplot.js');
 
-var app = angular.module('helloapp', [wiButton.name, wiToolbar.name, wiLogplot.name, wiSlidingbar.name]);
+var app = angular.module('helloapp', [wiLogplot.name]);
 app.controller('WiDummy', function ($scope) {
 
 });
--- source/app/components/wi-logplot/wi-logplot.js
+++ source/app/components/wi-logplot/wi-logplot.js
@@ -1,12 +1,16 @@
 const componentName = 'wiLogplot';
 const moduleName = 'wi-logplot';
 
-function Controller() {
-    var self = this;
+wiButton = require('./wi-button.js');
+wiToolbar = require('./wi-toolbar.js');
+wiSlidingbar = require('./wi-slidingbar.js');
 
+function Controller(wiSlidingbar) {
+    var self = this;
+    this.wiSlidingbar = wiSlidingbar;
 }
 
-var app = angular.module(moduleName, []);
+var app = angular.module(moduleName, [wiButton.name, wiToolbar.name, wiSlidingbar.name]);
 app.component(componentName, {
     templateUrl: 'wi-logplot.html',
     controller: Controller,
