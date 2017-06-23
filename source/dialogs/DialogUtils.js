b075bd9c0772e2f32447fc6d95954ce637e3443d
modao <modaouet1012@gmail.com>
2017-06-23 09:11:16 +0700

    Modify unit seeting modal

--- source/dialogs/DialogUtils.js
+++ source/dialogs/DialogUtils.js
@@ -85,13 +85,197 @@ exports.confirmDialog = function(ModalService, titleMessage, confirmMessage, cal
     });
 }
 
-exports.unitSettingDialog = function(ModalService, callback, data) {
+exports.unitSettingDialog = function(ModalService, callback) {
     function ModalController($scope, close) {
+        this.defaultData = {
+            Default : {
+                unitSystem: "Default",
+                caliper : "in", 
+                neutron : "v/v",
+                gammaRay : "api",
+                acoustic : "us/ft",
+                pressure : "psi",
+                bitSize : "in",
+                density : "g/cm3",
+                concentration : "v/v",
+                permeability : "mD",
+                porosity : "v/v",
+                angle : "deg",
+                resistivity : "ohm.m",
+                saturation : "v/v",
+                temperature : "degC",
+                volume : "v/v",
+                sp : "mv",
+                length : "m",
+                time : "s",
+                area : "m2",
+                flow : "t",
+                speed : "m/s",
+                force : "N"    
+            },
+            Canadian : {
+                unitSystem : "Canadian",
+                caliper : "cm", 
+                neutron : "%",
+                gammaRay : "api",
+                acoustic : "us/m",
+                pressure : "psi",
+                bitSize : "cm",
+                density : "kg/m3",
+                concentration : "ppm",
+                permeability : "mD",
+                porosity : "v/v",
+                angle : "deg",
+                resistivity : "ohm.m",
+                saturation : "v/v",
+                temperature : "degC",
+                volume : "v/v",
+                sp : "mv",
+                length : "m",
+                time : "s",
+                area : "m2",
+                flow : "t",
+                speed : "m/s",
+                force : "N"    
+            },
+            English : {
+                unitSystem : "English",
+                caliper : "in", 
+                neutron : "%",
+                gammaRay : "api",
+                acoustic : "us/ft",
+                pressure : "psi",
+                bitSize : "in",
+                density : "g/cm3",
+                concentration : "ppm",
+                permeability : "mD",
+                porosity : "v/v",
+                angle : "deg",
+                resistivity : "ohm.m",
+                saturation : "v/v",
+                temperature : "degC",
+                volume : "v/v",
+                sp : "mv",
+                length : "m",
+                time : "s",
+                area : "m2",
+                flow : "t",
+                speed : "m/s",
+                force : "N"    
+            },
+            Metric : {
+                unitSystem : "Metric",
+                caliper : "cm", 
+                neutron : "%",
+                gammaRay : "api",
+                acoustic : "us/ft",
+                pressure : "mBar",
+                bitSize : "cm",
+                density : "g/cm3",
+                concentration : "ppm",
+                permeability : "mD",
+                porosity : "v/v",
+                angle : "deg",
+                resistivity : "ohm.m",
+                saturation : "v/v",
+                temperature : "degC",
+                volume : "v/v",
+                sp : "mv",
+                length : "m",
+                time : "s",
+                area : "m2",
+                flow : "t",
+                speed : "m/s",
+                force : "N"    
+            },
+            Russian : {
+                unitSystem : "Russian",
+                caliper : "cm", 
+                neutron : "%",
+                gammaRay : "api",
+                acoustic : "us/ft",
+                pressure : "mBar",
+                bitSize : "cm",
+                density : "g/cm3",
+                concentration : "ppm",
+                permeability : "mD",
+                porosity : "v/v",
+                angle : "deg",
+                resistivity : "ohm.m",
+                saturation : "v/v",
+                temperature : "degC",
+                volume : "v/v",
+                sp : "mv",
+                length : "m",
+                time : "s",
+                area : "m2",
+                flow : "t",
+                speed : "m/s",
+                force : "N"                 
+            }
+        };
+        this.allData = {
+            unitSystem : ["Default", "Canadian", "English", "Metric", "Russian"],
+            caliper : ["in", "m", "cm", "Ft", "1.Ft", "mm", "um"],
+            neutron : ["v/v", "Trac", "%", "pu", "imp/min"],
+            gammaRay : ["api", "GAPI", "uR/h", "GAMA"], 
+            acoustic : ["us/ft", "us/m"],
+            pressure : ["psi", "Pa", "kPa", "MPa", "mBar", "Bar", "kg/m2", "atm", "torr"],
+            bitSize : ["in", "m", "cm", "Ft", "1.Ft", "mm", "um"],
+            density : ["g/cm3", "kg/m3"],
+            concentration : ["v/v", "%", "ppm", "kpp", "m", "1/L", "mS/m", "1/kg", "dB/m", "mV", "galUS/min", "mD/cP", "b/elec", "b/cm3", "m3/d", "MV"],
+            permeability : ["mD", "D"],
+            porosity : ["v/v", "m3/m3", "ft3/ft3", "%", "imp/min", "ratio"],
+            angle : ["deg", "dega", "grad", "rad"],
+            resistivity : ["ohm.m", "ratio"],
+            saturation : ["v/v", "m3/m3", "ft3/ft3",  "%", "ratio"],
+            temperature : ["degC", "degF"],
+            volume : ["v/v", "cm3", "L.m"],
+            sp : ["mv"],
+            length : ["m"],
+            time : ["s"],
+            area : ["m2"],
+            flow : ["t"],
+            speed : ["m/s", "m2", "ft/h", "ratio", "ft/s", "m/min", "rpm", "mn/m"],
+            force : ["N"]
+        };
+        function copyObj(sourceObj, destObj) {
+            for( var attr in sourceObj ) {
+                destObj[attr] = sourceObj[attr];
+            }
+        };
+        this.selectedData = {};
+        var self = this;
+        copyObj(self.defaultData.Default, self.selectedData);
+        this.setDefault = function(){
+            copyObj(self.defaultData.Default, self.selectedData);
+        };
+        this.changeDefault = function(){
+            switch (self.selectedData.unitSystem) {
+                case "Default":
+                    copyObj(self.defaultData.Default, self.selectedData);
+                    break;
+                case "Canadian":
+                    copyObj(self.defaultData.Canadian, self.selectedData);
+                    break;
+                case "English":
+                    copyObj(self.defaultData.English, self.selectedData);
+                    break;
+                case "Metric":
+                    copyObj(self.defaultData.Metric, self.selectedData);
+                    break;
+                case "Russian":
+                    copyObj(self.defaultData.Russian, self.selectedData);
+                    break;
+                default:
+                    console.log("Error: NULL");
+                    break;
+            }
+        };
+        console.log(self.selectedData.unitSystem)
         this.close = function(ret) {
             close(ret);
-        }
-
-        this.data = data;
+        }                    
     }
     ModalService.showModal({
         templateUrl: "unit-setting/unit-setting-modal.html",
@@ -127,3 +311,4 @@ exports.addNewDialog = function(ModalService, callbak) {
         });
     });
 }
+
