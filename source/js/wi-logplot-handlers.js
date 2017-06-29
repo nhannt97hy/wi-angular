f812af3af2e8a0f77a2ae63c30c8e645c3640353
minhnt58 <tienminh.uet@gmail.com>
2017-06-29 12:04:09 +0700

    push di nhan bang

--- source/js/wi-logplot-handlers.js
+++ source/js/wi-logplot-handlers.js
@@ -1,5 +1,32 @@
+function genSamples(nSamples) {
+    var samples = new Array();
+    for( let i = 0; i < nSamples; i++ ) {
+        samples.push({y:i, x: Math.random()});
+    }
+    return samples;
+}
+
 exports.Button1ButtonClicked = function() {
     console.log('Button1Button is clicked');
+
+    var myPlot = this.wiComponentService.getComponent('myLogPlotD3Area');
+    if (!myPlot) return;
+    var slidingBar = this.wiComponentService.getComponent('myLogPlotSlidingbar');
+    if (!slidingBar) return;
+
+    var idx = myPlot.addDepthTrack();
+
+    idx = myPlot.addTrack();
+
+    myPlot.setData(idx, genSamples(10000));
+
+    var maxDepth = myPlot.getMaxDepth();
+
+    var low = slidingBar.slidingBarState.top * maxDepth / 100;
+    var high = (slidingBar.slidingBarState.top + slidingBar.slidingBarState.range) * maxDepth / 100;
+    console.log(slidingBar.slidingBarState, low, high, maxDepth);
+    myPlot.setDepthRange([low, high]);
+    myPlot.plotAll();
 }
 
 exports.Button2ButtonClicked = function() {
