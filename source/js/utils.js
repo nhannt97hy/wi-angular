a04178b8da4882935b3691167e883cb74825e79f
modao <modaouet1012@gmail.com>
2017-07-11 10:28:32 +0700

    open-project-modal: disabled input info project

--- source/js/utils.js
+++ source/js/utils.js
@@ -47,6 +47,7 @@ function projectOpen(wiComponentService, projectData) {
 function projectClose(wiComponentService) {
     wiComponentService.emit('project-unloaded-event');
     openProject.valid = false;
+    openProject = {};
 }
 
 exports.objcpy = objcpy;
