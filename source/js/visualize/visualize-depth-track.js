0e1de3ef81d1096bda2c0e35822d3f7ec44a382e
canhnd58 <canhnd_58@vnu.edu.vn>
2017-10-30 12:18:46 +0700

    Fix logplot scale rounding error

--- source/js/visualize/visualize-depth-track.js
+++ source/js/visualize/visualize-depth-track.js
@@ -115,7 +115,8 @@ DepthTrack.prototype.doPlot = function(highlight) {
 
 
     // let step = Math.pow(10, Math.round(Math.log((windowY[1] - windowY[0]) / this.yTicks) / Math.log(10))) * 2;
-    let step = transformY.invert(Utils.getDpcm()) - windowY[0];
+    let dpcm = Utils.getDpcm();
+    let step = transformY.invert(dpcm) - windowY[0];
     let start = Utils.roundUp(windowY[0], step);
     let end = Utils.roundDown(windowY[1], step);
     let yAxisRight = d3.axisLeft(transformY)
