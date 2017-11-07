8a6ba4ebac3e1cb261d0a43375a8b12bb754a027
DTM1012 <modaouet1012@gmail.com>
2017-11-07 12:00:54 +0700

    Nhan update split curve

--- source/dialogs/DialogUtils.js
+++ source/dialogs/DialogUtils.js
@@ -8190,53 +8190,176 @@ exports.curveConvolutionDialog = function(ModalService){
 }
 
 exports.splitCurveDialog = function (ModalService, callback) {
-    function ModalController($scope, wiComponentService, wiApiService, close) {
+    function ModalController($scope, wiComponentService, wiApiService, close, $timeout) {
         let self = this;
         window.split = this;
         let utils = wiComponentService.getComponent(wiComponentService.UTILS);
         let dialogUtils = wiComponentService.getComponent(wiComponentService.DIALOG_UTILS);
         let wiExplorer = wiComponentService.getComponent(wiComponentService.WI_EXPLORER);
 
+        this.process = false;
+
+        this.disable = true;
         this.wells = wiExplorer.treeConfig[0].children;
+        this.idWell; this.idCurve; this.idDataset;
+        this.datasetModel;
         this.curves = [];
-        this.topDepth;
-        this.bottomDepth;
+        this.datasets = [];
+        this.topDepth; this.bottomDepth;
+        this.numberSplit = 1;
+        this.number = this.numberSplit;
+        this.data;
+        this.arrayCurve = [];
         let selectedNodes = wiComponentService.getComponent(wiComponentService.SELECTED_NODES);
-        if( selectedNodes) {
-            if( selectedNodes[0].type == 'well' )
-                this.wellModel = selectedNodes[0];
-            if( selectedNodes[0].type == 'curve' ) {
-                this.curveModel = selectedNodes[0];
-                this.wellModel = utils.findWellByCurve(self.curveModel.id);
-            }
+        if( selectedNodes && selectedNodes[0].type == 'well')
+            this.wellModel = selectedNodes[0];
+        else if( selectedNodes && selectedNodes[0].type == 'curve' ) {
+            this.curveModel = selectedNodes[0];
+            this.wellModel = utils.findWellByCurve(this.curveModel.id);
         }
         else {
             this.wellModel = angular.copy(self.wells[0]);
-            this.curveModel = self.wellModel.children[0].children[0];
         }
-        function depthInfo() {
+
+        getInfo();
+        pushArrayCurve();
+        function getAllCurve() {
+            self.datasets = [];
+            self.curves = [];
+            self.wellModel.children.forEach(function(child) {
+                if(child.type == 'dataset') {
+                    self.datasets.push(child);
+                    child.children.forEach(function(item) {
+                        if(item.type == 'curve')
+                            self.curves.push(item);
+                    });
+                }
+            });
+        }
+        function getInfo() {
+            getAllCurve();
+            self.idWell = self.wellModel.id;
+            self.datasetModel = self.datasets[0];
+            self.idDataset = self.datasetModel.id;
+            self.curveModel = self.curves[0];
+            self.idCurve = self.curveModel.id;
             self.topDepth = parseFloat(self.wellModel.properties.topDepth);
             self.bottomDepth = parseFloat(self.wellModel.properties.bottomDepth);
-            self.curves = self.wellModel.children[0].children;
-        }
-        depthInfo();
+        };
         this.selectWell = function (idWell) {
             self.wellModel = utils.findWellById(idWell);
-            depthInfo();
-            self.curveModel =self.curves[0];
+            self.curveModel = self.wellModel.children[0].children[0];
+            getInfo();
+            self.arrayCurve.forEach(function( curve ) {
+                curve.start = self.topDepth;
+                curve.end = self.bottomDepth;
+            });
         }
         this.selectCurve = function (idCurve) {
             self.curveModel = utils.getCurveFromId(idCurve);
         }
+        this.selectDataset = function (idDataset) {
+            self.datasetModel = utils.findDatasetById(idDataset);
+            self.idDataset = self.datasetModel.id;
+        }
+        this.changeNumberSplit = changeNumberSplit;
+        function pushArrayCurve() {
+            self.arrayCurve.push({
+                name: null,
+                start: self.topDepth,
+                end: self.bottomDepth,
+                data: []
+            });
+        }
+        function changeNumberSplit (numberSplit) {
+            if(self.number <= numberSplit) {
+                for(var i = self.number + 1; i <= numberSplit; i++)
+                    pushArrayCurve();
+            } else {
+                self.arrayCurve.splice(numberSplit, self.number - numberSplit);
+            }
+            self.number = numberSplit;
+        }
+        function refresh() {
+            self.wellModel = utils.findWellById(self.idWell);
+            getInfo();
+        }
+        wiComponentService.on(wiComponentService.PROJECT_REFRESH_EVENT, function() {
+            self.process = false;
+            $timeout(function(){
+                refresh();
+            }, 100);
+        });
 
-        this.numberSplit = 1;
+        function check() {
+            let valid = true;
+            self.arrayCurve.forEach(function(curve){
+                if(curve.start > curve.end ||
+                    curve.start < self.topDepth ||
+                    curve.end > self.bottomDepth) valid = false;
+            });
+            console.log("vvv", valid);
+            return valid;
+        }
+        this.onRunButtonClicked = function () {
+            if(self.process)
+                return;
+            self.process = true;
+            let temp = check();
+            if (!temp) {
+                dialogUtils.errorMessageDialog(ModalService, "Depth Start or Depth End is invalid!", function(){
+                    self.process = true;
+                });
+            } else {
+                wiApiService.dataCurve(self.curveModel.id, function(dataCurve) {
+                    console.log(dataCurve);
 
+                    async.each(self.arrayCurve,function(curve, cb) {
+                        dataCurve.forEach(function(data, y) {
+                            let currentDept = y*parseFloat(self.wellModel.properties.step) + parseFloat(self.wellModel.properties.topDepth);
+
+                            if(currentDept >= curve.start && currentDept <= curve.end) {
+                                curve.data[y] = parseFloat(data.x);
+                            }else {
+                                curve.data[y] = NaN;
+                            }
+                        });
+                        cb();
+                    }, function (err) {
+                        async.each(self.arrayCurve, function(curve, cb) {
+                            let payload = {
+                                idDataset: self.idDataset,
+                                curveName: curve.name,
+                                unit: self.curveModel.properties.unit,
+                                data: curve.data
+                            };
+                            wiApiService.processingDataCurve(payload, cb);
+                        }, function(err) {
+                            console.log('done');
+                            utils.refreshProjectState();
+                        });
+                    });
+                });
+            }
+
+        }
         this.onCancelButtonClicked = function () {
             close(null, 100);
         };
     }
     ModalService.showModal({
         "templateUrl": "split-curve/split-curve-modal.html",
+        controller: ModalController,
+        controllerAs: 'wiModal'
+    }).then(function (modal) {
+        modal.element.modal();
+        $(modal.element[0].children[0]).draggable();
+        modal.close.then(function () {
+            $('.modal-backdrop').remove();
+            $('body').removeClass('modal-open');
+        });
+    });
+}
 
 exports.fillDataGapsDialog = function(ModalService){
     function ModalController(wiComponentService, wiApiService, close){
@@ -8535,25 +8658,14 @@ exports.curveDerivativeDialog = function(ModalService){
 
     ModalService.showModal({
         templateUrl: "curve-derivative/curve-derivative-modal.html",
->>>>>>> 94e765e8b6d452eadbe0533171e373497c159f0a
         controller: ModalController,
         controllerAs: 'wiModal'
     }).then(function (modal) {
         modal.element.modal();
         $(modal.element[0].children[0]).draggable();
-<<<<<<< HEAD
-        modal.close.then(function (ret) {
-            $('.modal-backdrop').remove();
-            $('body').removeClass('modal-open');
-            callback(ret);
-        });
-    });
-}
-=======
         modal.close.then(function () {
             $('.modal-backdrop').remove();
             $('body').removeClass('modal-open');
         });
     });
 }
->>>>>>> 94e765e8b6d452eadbe0533171e373497c159f0a
