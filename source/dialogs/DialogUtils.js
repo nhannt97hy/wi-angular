a293685a828d440ccee61e7d0529e1670e771eb3
Cuong Nguyen Duy <cuongnd95@live.com>
2017-11-23 17:01:17 +0700

    update tvd-conversion

--- source/dialogs/DialogUtils.js
+++ source/dialogs/DialogUtils.js
@@ -9374,7 +9374,7 @@ exports.TVDConversionDialog = function (ModalService) {
     function ModalController(wiComponentService, wiApiService, close, $timeout) {
         let self = this;
         window.tvd = this;
-
+        this.applyingInProgress = false;
         let utils = wiComponentService.getComponent(wiComponentService.UTILS);
         let dialogUtils = wiComponentService.getComponent(wiComponentService.DIALOG_UTILS);
         let wiExplorer = wiComponentService.getComponent(wiComponentService.WI_EXPLORER);
@@ -9387,6 +9387,7 @@ exports.TVDConversionDialog = function (ModalService) {
         this.useType = 'file';
         this.startData = 2, this.colDepth = 1, this.colDev = 2, this.colAzi = 3;
         this.tvdMethod = 'off', this.calMethod = '1';
+        this.elevation = 0, this.xRef = 0, this.yRef = 0;
         this.input = [];
         this.curvesData = [];
 
@@ -9446,8 +9447,6 @@ exports.TVDConversionDialog = function (ModalService) {
             self.topDepth = parseFloat(self.SelectedWell.properties.topDepth);
             self.bottomDepth = parseFloat(self.SelectedWell.properties.bottomDepth);
             self.tvdRef = parseFloat(self.SelectedWell.properties.topDepth);
-            let length = Math.round((self.bottomDepth - self.topDepth)/self.step) + 1;
-            self.Size = new Array(length);
         }
         this.onChangeWell();
         this.loadFile = function(){
@@ -9458,20 +9457,28 @@ exports.TVDConversionDialog = function (ModalService) {
                     let reader = new FileReader();
                     reader.onload = function(event){
                         let lines = this.result.split('\n');
+                        let max = -1;
                         for(let i = 0; i < lines.length; i++){
                             let ele = lines[i].replace(/\(|\)/g,'').replace(/ /g, '').split(/\s+/);
                             if(i > 0) ele.pop();
+                            max = max > ele.length ? max : ele.length;
                             if(ele.length) self.input.push(ele);
                         }
+                        self.input.forEach(function(d){
+                            d.length = max;
+                        })
                     }
                     reader.readAsText(self.SurveyFile);
                 }else{
-                    utils.error('File not supported!');
+                    utils.error('Supported text file(.txt) only!');
+                    delete self.SurveyFile;
                 }
             }
         }
         this.loadCurves = function(){
             console.log('Load Curves');
+            let length = Math.round((self.bottomDepth - self.topDepth)/self.step) + 1;
+            self.Size = new Array(length);
             function loadAzi(){
                 let azi = self.curvesData.find(curve => {return curve.id == self.AziCurve.id});
                 if(azi){
@@ -9516,9 +9523,199 @@ exports.TVDConversionDialog = function (ModalService) {
             }
         }
 
-        $('#inputDiv').scroll(function(){
-            $('#inputTable').width($('#inputDiv').width() + $('#inputDiv').scrollLeft());
-        })
+        function formula1(){
+            console.log('Average Angle');
+            for(let i = 1; i < self.Size.length; i++){
+                let md;
+                if(self.useType == 'file'){
+                    md = self.depthArr[i] - self.depthArr[i - 1];
+                }else{
+                    md = self.step;
+                }
+                let dev = ((self.devArr[i - 1] + self.devArr[i])/2) * Math.PI / 180 ;
+                let azi = ((self.aziArr[i - 1] + self.aziArr[i])/2) * Math.PI / 180;
+                let n = md * Math.sin(dev) * Math.cos(azi);
+                let e = md * Math.sin(dev) * Math.sin(azi);
+                let tvd = md * Math.cos(dev);
+
+                self.tvdArr[i] = self.tvdArr[i - 1] + tvd;
+                self.northArr[i] = self.northArr[i - 1] + n;
+                self.eastArr[i] = self.eastArr[i - 1] + e;
+                // self.xArr[i] = self.xArr[i - 1] + e;
+                // self.yArr[i] = self.yArr[i - 1] + n;
+            }
+            if(self.tvdMethod == 'off'){
+                self.tvdssArr = self.tvdArr.map(d => {return d - self.elevation;});
+            }else{
+                self.tvdssArr = self.tvdArr.map(d => {return self.elevation - d;});                
+            }
+        }
+        function formula2(){
+            console.log('Balanced Tangential');
+            for(let i = 1; i < self.Size.length; i++){
+                let md;
+                if(self.useType == 'file'){
+                    md = self.depthArr[i] - self.depthArr[i - 1];
+                }else{
+                    md = self.step;
+                }
+                let dev1 = self.devArr[i - 1] * Math.PI / 180 ;
+                let dev2 = self.devArr[i] * Math.PI / 180 ;
+                let azi1 = self.aziArr[i - 1] * Math.PI / 180;
+                let azi2 = self.aziArr[i] * Math.PI / 180;
+                let n = md/2 * (Math.sin(dev1) * Math.cos(azi1) + Math.sin(dev2) * Math.cos(azi2));
+                let e = md/2 * (Math.sin(dev1) * Math.sin(azi1) + Math.sin(dev2) * Math.sin(azi2));
+                let tvd = md/2 * (Math.cos(dev1) + Math.cos(dev2));
+
+                self.tvdArr[i] = self.tvdArr[i - 1] + tvd;
+                self.northArr[i] = self.northArr[i - 1] + n;
+                self.eastArr[i] = self.eastArr[i - 1] + e;
+                // self.xArr[i] = self.xArr[i - 1] + e;
+                // self.yArr[i] = self.yArr[i - 1] + n;
+            }
+            if(self.tvdMethod == 'off'){
+                self.tvdssArr = self.tvdArr.map(d => {return d - self.elevation;});
+            }else{
+                self.tvdssArr = self.tvdArr.map(d => {return self.elevation - d;});                
+            }
+        }
+        function formula3(){
+            console.log('Radius Curvature');
+            for(let i = 1; i < self.Size.length; i++){
+                let md;
+                if(self.useType == 'file'){
+                    md = self.depthArr[i] - self.depthArr[i - 1];
+                }else{
+                    md = self.step;
+                }
+                let dev1 = self.devArr[i - 1] * Math.PI / 180 ;
+                let dev2 = self.devArr[i] * Math.PI / 180 ;
+                let azi1 = self.aziArr[i - 1] * Math.PI / 180;
+                let azi2 = self.aziArr[i] * Math.PI / 180;
+                let n = md * (Math.cos(dev1) - Math.cos(dev2)) * (Math.sin(azi2) - Math.sin(azi1)) * Math.pow(180/Math.PI, 2)/((self.devArr[i] - self.devArr[i - 1])*(self.aziArr[i] - self.aziArr[i - 1]));
+                let e = md * (Math.cos(dev1) - Math.cos(dev2)) * (Math.cos(azi1) - Math.cos(azi2)) * Math.pow(180/Math.PI, 2)/((self.devArr[i] - self.devArr[i - 1])*(self.aziArr[i] - self.aziArr[i - 1]));
+                let tvd = md * (Math.sin(dev2) - Math.sin(dev1)) * (180/Math.PI) /(self.devArr[i] - self.devArr[i - 1]);
+
+                self.tvdArr[i] = self.tvdArr[i - 1] + tvd;
+                self.northArr[i] = self.northArr[i - 1] + n;
+                self.eastArr[i] = self.eastArr[i - 1] + e;
+                // self.xArr[i] = self.xArr[i - 1] + e;
+                // self.yArr[i] = self.yArr[i - 1] + n;
+            }
+            if(self.tvdMethod == 'off'){
+                self.tvdssArr = self.tvdArr.map(d => {return d - self.elevation;});
+            }else{
+                self.tvdssArr = self.tvdArr.map(d => {return self.elevation - d;});                
+            }
+        }
+        function formula4(){
+            console.log('Minimum Curvature');
+            for(let i = 1; i < self.Size.length; i++){
+                let md;
+                if(self.useType == 'file'){
+                    md = self.depthArr[i] - self.depthArr[i - 1];
+                }else{
+                    md = self.step;
+                }
+                let dev1 = self.devArr[i - 1] * Math.PI / 180 ;
+                let dev2 = self.devArr[i] * Math.PI / 180 ;
+                let azi1 = self.aziArr[i - 1] * Math.PI / 180;
+                let azi2 = self.aziArr[i] * Math.PI / 180;
+                let b = Math.acos((Math.cos(dev2 - dev1) - Math.sin(dev1) * Math.sin(dev2) * (1 - Math.cos(azi2-azi1))) * Math.PI / 180);
+                let RF = 2/b * Math.tan(b/2 * Math.PI / 180);
+                let n = md/2 * (Math.sin(dev1) * Math.cos(azi1) + Math.sin(dev2) * Math.cos(azi2)) * RF;
+                let e = md/2 * (Math.sin(dev1) * Math.sin(azi1) + Math.sin(dev2) * Math.sin(azi2)) * RF;
+                let tvd = md/2 * (Math.cos(dev1) + Math.cos(dev2)) * RF;
+
+                self.tvdArr[i] = self.tvdArr[i - 1] + tvd;
+                self.northArr[i] = self.northArr[i - 1] + n;
+                self.eastArr[i] = self.eastArr[i - 1] + e;
+                // self.xArr[i] = self.xArr[i - 1] + e;
+                // self.yArr[i] = self.yArr[i - 1] + n;
+            }
+            if(self.tvdMethod == 'off'){
+                self.tvdssArr = self.tvdArr.map(d => {return d - self.elevation;});
+            }else{
+                self.tvdssArr = self.tvdArr.map(d => {return self.elevation - d;});                
+            }
+        }
+        
+        this.onRunButtonClicked = function(){
+            if(self.applyingInProgress) return;
+            self.applyingInProgress = true;
+            
+            if(self.useType == 'file'){
+                if(self.SurveyFile){
+                    let length = self.input.length - self.startData;
+                    self.Size = new Array(length);
+                    self.depthArr = new Array(length);
+                    self.devArr = new Array(length);
+                    self.aziArr = new Array(length);
+                    self.tvdArr = new Array(length);
+                    self.tvdssArr = new Array(length);
+                    self.northArr = new Array(length);
+                    self.eastArr = new Array(length);
+                    // self.xArr = new Array(length);
+                    // self.yArr = new Array(length);
+                    // self.radiusArr = new Array(length);
+
+                    let first = parseFloat(isNaN(self.input[self.startData][self.colDepth - 1]) ? 0 : self.input[self.startData][self.colDepth - 1]);
+                    self.depthArr[0] = self.tvdRef <  first ? self.tvdRef : first;
+                    for(let i = 1; i < length; i++){
+                        let d = self.input[i + self.startData][self.colDepth - 1];
+                        self.depthArr[i] = parseFloat(d == undefined || isNaN(d) ? 0 : d);
+                    }
+                    for(let j = 0; j < length; j++){
+                        let d = self.input[j + self.startData][self.colDev - 1];
+                        let a = self.input[j + self.startData][self.colAzi - 1];
+                        self.devArr[j] = parseFloat(d == undefined || isNaN(d) ? 0 : d);
+                        self.aziArr[j] = parseFloat(a == undefined || isNaN(a) ? 0 : a);
+                    }
+                }else{
+                    utils.error('Please open supported survey file!');
+                    self.applyingInProgress = false;
+                }
+            }else{
+                let length = self.Size.length;
+                self.depthArr = new Array(length);
+                self.tvdArr = new Array(length);
+                self.tvdssArr = new Array(length);
+                self.northArr = new Array(length);
+                self.eastArr = new Array(length);
+                // self.xArr = new Array(length);
+                // self.yArr = new Array(length);
+                // self.radiusArr = new Array(length);
+                self.depthArr[0] = self.tvdRef < self.topDepth ? self.tvdRef : self.topDepth;
+                for(let i = 1; i < length; i++){
+                    self.depthArr[i] = i * self.step + self.topDepth;
+                }
+            }
+            self.tvdArr[0] = self.depthArr[0];
+            self.northArr[0] = 0;
+            self.eastArr[0] = 0;
+            // self.xArr[0] = self.xRef;
+            // self.yArr[0] = self.yRef;
+            // self.radiusArr[0] = 0;
+
+            switch (self.calMethod){
+                case '1':
+                formula1();
+                break;
+
+                case '2':
+                formula2();
+                break;
+
+                case '3':
+                formula3();
+                break;
+
+                case '4':
+                formula4();
+                break;
+            }
+        }
+
         this.onCancelButtonClicked = function(){
             close(null);
         }
