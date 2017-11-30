4b5752d0f4afd50cbb0a9c1afbfa2c67cb23ab1d
DTM1012 <modaouet1012@gmail.com>
2017-11-30 15:33:43 +0700

    fix delete on reference windows table

--- source/dialogs/DialogUtils.js
+++ source/dialogs/DialogUtils.js
@@ -7425,12 +7425,14 @@ exports.referenceWindowsDialog = function (ModalService, well, plotModel, callba
         this.Delete = function (index) {
             self.SelectedRefCurve = index;
             // self.ref_Curves_Arr.splice(self.SelectedRefCurve, index, 1);
-            // if (self.ref_Curves_Arr[self.SelectedRefCurve].flag != self._FNEW) {
-            //     self.ref_Curves_Arr[self.SelectedRefCurve].flag = self._FDEL;
-            // } else {
+            if (self.ref_Curves_Arr[self.SelectedRefCurve].flag != self._FNEW) {
+                self.ref_Curves_Arr[self.SelectedRefCurve].flag = self._FDEL;
                 self.ref_Curves_Arr.splice(self.SelectedRefCurve, 1);
-            // }
+            } else {
+                self.ref_Curves_Arr.splice(self.SelectedRefCurve, 1);
+            }
             self.SelectedRefCurve = self.SelectedRefCurve > 0 ? self.SelectedRefCurve - 1 : -1;
+            // $event.stopPropagation();
         }
 
         this.DeleteRefCurve = function(){
