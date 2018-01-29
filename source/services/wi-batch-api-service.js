85ed5f7d69bda917806c1645f5be8f7c484c7e6e
Tung HOANG <tung.hoang@gmail.com>
2018-01-29 18:12:44 +0700

    update

---
+++ source/services/wi-batch-api-service.js
@@ -0,0 +1,57 @@
+const wiServiceName = 'wiBatchApiService';
+const moduleName = 'wi-batch-api-service';
+
+const wiBatchURL = "http://192.168.0.91:33333";
+
+const LIST_WF = wiBatchURL + "/workflow/list";
+const DELETE_DATADIR = wiBatchURL + "/workflow/delete-data";
+let app = angular.module(moduleName, []);
+app.factory(wiServiceName, function ($http) {
+    return new WiBatchApi($http);
+});
+
+function WiBatchApi($http) {
+    this.$http = $http;
+}
+
+WiBatchApi.prototype.listWorkflows = function(callback) {
+    this.$http({
+        method: 'POST',
+        url: LIST_WF,
+        headers: {
+            Authorization: window.localStorage.getItem('token')
+        }
+    }).then(function(response) {
+        if (response.data.code != 200) {
+            callback(new Error(response.data.reason));
+        }
+        else {
+            callback(null, response.data.content);
+        }
+    }, function(err) {
+        callback(err);
+    });
+}
+WiBatchApi.prototype.deleteDataDir = function(dataDir, callback) {
+    this.$http({
+        method: 'POST',
+        url: DELETE_DATADIR,
+        headers: {
+            Authorization: window.localStorage.getItem('token')
+        },
+        data: {
+            dataDir: dataDir
+        }
+    }).then(function(response) {
+        if (response.data.code != 200) {
+            callback(new Error(response.data.reason));
+        }
+        else {
+            callback(null, response.data.content);
+        }
+    }, function(err) {
+        callback(err);
+    });
+}
+WiBatchApi.prototype.getWiBatchUrl = function() { return wiBatchURL; }
+exports.name = moduleName;
