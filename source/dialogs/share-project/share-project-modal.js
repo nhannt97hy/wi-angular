3066e93d7314d402e7469a1582291be2566ddc79
hoang <bdhoang.ht@gmail.com>
2018-03-30 10:41:13 +0700

    style lai

--- source/dialogs/share-project/share-project-modal.js
+++ source/dialogs/share-project/share-project-modal.js
@@ -8,25 +8,48 @@ module.exports = function (ModalService, callback, groups, users) {
         this.users = users;
         this.selectedGroup = null;
         this.selectedUser = null;
-        this.showUserList = true;
         this.isSelectedGroup = false;
         this.isSelectedUser = false;
         this.idGroups = [];
+
+        preGroup();
+
+        function preUser(group) {
+            self.users.forEach(user => {
+                let tmp = group.users.find(u => u.idUser === user.idUser);
+                user.isShared = !!tmp;
+            });
+        }
+
+        function preGroup() {
+            self.groups.forEach(function (group) {
+                let tmp = group.shared_projects.find(s => s.project_name === self.project.name);
+                if (tmp) {
+                    group.isShared = true;
+                    group.idSharedProject = tmp.idSharedProject;
+                } else {
+                    group.isShared = false;
+                }
+            });
+        }
 
         function reload() {
             wiApiService.listGroup({}, function (groups) {
                 $timeout(function () {
                     self.groups = groups;
+                    preGroup();
                     if (!self.selectedGroup) return;
 
                     self.selectedGroup = self.groups.find(g => g.idGroup === self.selectedGroup.idGroup);
-                    self.users = users.filter(
-                        u => !self.selectedGroup.users.find(uInSelectdGroup => uInSelectdGroup.idUser === u.idUser)
-                    );
+                    if (self.selectedGroup) preUser(self.selectedGroup);
                 });
             });
         }
 
+        this.groupClicked = function (group) {
+            self.selectedGroup = group;
+            preUser(group);
+        };
         this.addNewGroup = function () {
             let promptConfig = {
                 title: 'Add new user group',
@@ -39,40 +62,67 @@ module.exports = function (ModalService, callback, groups, users) {
                 });
             });
         };
-        this.deleteGroup = function () {
-            wiApiService.removeGroup({idGroup: self.selectedGroup.idGroup}, function (response) {
-                self.selectedGroup = null;
+        this.deleteGroup = function (group) {
+            wiApiService.removeGroup({idGroup: group.idGroup}, function (response) {
+                if (self.selectedGroup && self.selectedGroup.idGroup === group.idGroup) self.selectedGroup = null;
+                toastr.success("Successfull delete group " + group.name);
                 reload();
             });
-        };
-        this.addUserToGroup = function () {
-            self.showUserList = !self.showUserList;
         };
         this.addUser = function (user) {
             wiApiService.addUserToGroup({
                 idGroup: self.selectedGroup.idGroup,
-                idUsers: [user.idUser]
+                idUser: user.idUser
             }, function () {
+                toastr.success("Successfull add " + user.username + " to group " + self.selectedGroup.name);
                 reload();
             })
         };
-        this.removeUser = function () {
+        this.removeUser = function (user) {
             wiApiService.removeUserFromGroup({
                 idGroup: self.selectedGroup.idGroup,
-                idUser: self.selectedUser.idUser
-            }, function () {
+                idUser: user.idUser
+            }, function (response) {
+                if (response === "CANT_REMOVE_OWNER") {
+                    toastr.error("You can't remove group's owner from group");
+                } else {
+                    toastr.success("Successfull remove " + user.username + " from group " + self.selectedGroup.name);
+                }
                 reload();
             })
         };
-        this.onOkButtonClicked = function () {
+        this.shareProjectToGroup = function (group) {
             wiApiService.addSharedProject({name: self.project.name}, function (sharedProject) {
                 wiApiService.addProjectToGroup({
-                    idGroups: [self.selectedGroup.idGroup],
+                    type: "add",
+                    idGroup: group.idGroup,
                     idSharedProject: sharedProject.idSharedProject
                 }, function () {
-                    close(500);
+                    toastr.success("Successfull share " + self.project.name + " to group " + group.name);
+                    reload();
                 });
             });
+        };
+        this.unShareProjectFromGroup = function (group) {
+            wiApiService.addProjectToGroup({
+                type: "remove",
+                idGroup: group.idGroup,
+                idSharedProject: group.idSharedProject
+            }, function (response) {
+                toastr.success("Successfull stop sharing " + self.project.name + " from group " + group.name);
+                reload();
+            });
+        };
+        this.onOkButtonClicked = function () {
+            // wiApiService.addSharedProject({name: self.project.name}, function (sharedProject) {
+            //     wiApiService.addProjectToGroup({
+            //         idGroups: [self.selectedGroup.idGroup],
+            //         idSharedProject: sharedProject.idSharedProject
+            //     }, function () {
+            //         close(500);
+            //     });
+            // });
+            close(500);
         };
         this.onCancelButtonClicked = function () {
             close(null);
