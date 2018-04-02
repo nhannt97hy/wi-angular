40b23c0a223e2ea35a9d3d67087a285f050f28ef
nhannt97 <nhannt97hy@gmail.com>
2018-04-02 16:32:55 +0700

    update

--- source/dialogs/SimpleDialogUtils.js
+++ source/dialogs/SimpleDialogUtils.js
@@ -8,6 +8,4 @@ exports.warningMessageDialog = require('./warning-message-modal');
 exports.runImportDialog = require('./run-import-modal');
 exports.runImportWHDialog = require('./run-import-well-header-modal');
 exports.runImportWTDialog = require('./run-import-well-top-modal');
-exports.trainModelDialog = require('./train-model-modal');
-exports.predictModelDialog = require('./predict-model-modal');
 exports.neuralNetWorkPropertiesDialog = require('./neural-network-playground-modal');
