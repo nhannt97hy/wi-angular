228e8da114b4c50f545074f1812360bb1f713fd5
VuThuyThuy97 <vuthuy.uet@gmail.com>
2018-04-19 11:07:20 +0700

    update

--- source/js/handlers.js
+++ source/js/handlers.js
@@ -256,7 +256,7 @@ exports.ExportFromInventoryButtonClicked = function() {
             html: `
                 <div style='height:100%;display:flex;flex-direction:column;'>
                   
-                   <wi-export style='flex:1;'></wi-export>
+                   <wi-export style='flex:1;' from="inventory"></wi-export>
                 </div>
             `,
             name: 'wiExport'
@@ -265,7 +265,26 @@ exports.ExportFromInventoryButtonClicked = function() {
 
 }
 exports.ExportFromProjectButtonClicked = function (){
-        console.log('ExportFromProjectButton is clicked');
+    console.log('ExportFromProjectButton is clicked');
+    let self = this;
+    let wiComponentService = this.wiComponentService;
+    let utils = wiComponentService.getComponent(wiComponentService.UTILS);
+    let loadedProject = wiComponentService.getComponent(wiComponentService.PROJECT_LOADED);
+    const layoutManager = wiComponentService.getComponent(wiComponentService.LAYOUT_MANAGER);
+    layoutManager.putTabRight({
+        id: 'export-tool',
+        title: 'Export Tool',
+        tabIcon: 'export-well-top-16x16',
+        componentState: {
+            html: `
+                <div style='height:100%;display:flex;flex-direction:column;'>
+                  
+                   <wi-export style='flex:1;' from="project"></wi-export>
+                </div>
+            `,
+            name: 'wiExport'
+        }
+    })
 }
 exports.PropertyGridButtonClicked = function () {
     console.log('PropertyGridButton is clicked');
